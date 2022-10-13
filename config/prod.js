4ff6b48468c5e1e6f8c5b4f62586e8a20172c222
Sunmi Shin <sunmishin9559@gmail.com>
2022-10-13 09:29:50 -0500

    Configuration

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://summer:summer!1@boilerplate.p7slzs5.mongodb.net/?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://summer:summer!1@boilerplate.p7slzs5.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true, userCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log('err'))
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log('err'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.post('/register', (req, res) => {
+  // Save account information from client to DB
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});

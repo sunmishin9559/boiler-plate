4e02905d58a0202ad617e0e34a2604dfe597d1fb
Sunmi Shin <sunmishin9559@gmail.com>
2022-10-22 21:33:25 -0500

    Add login function

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require('./models/User');
 
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI)
@@ -21,13 +23,48 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => {
   // Save account information from client to DB
-  const user = new User(req.body)
+  const user = new User(req.body);
   user.save((err, userInfo) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
     })
   })
+});
+
+app.post('/login', (req, res) => {
+  //Check if user information exists
+  User.findOne({email: req.body.email}, (err, userInfo) => {
+    if(!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "No User Inforamtion Found."
+      })
+    }
+
+    //Check password validation
+    userInfo.validPassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "Invalid Password"
+        })
+      }
+
+      //Generate token
+      userInfo.generateToken((err, userInfo) => {
+        if(err) return res.status(400).send(err);
+
+        //Save token on Cookie
+        res.cookie("x_auth", userInfo.token)
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: userInfo._id
+        })
+      });
+    });
+  });
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -12,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 5
+        maxlength: 100
     },
     lastname: {
         type: String,
@@ -30,6 +33,47 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        //Encrypt the password
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+
+                user.password = hash;
+                next();
+            });
+        })
+    } else {
+        next();
+    }
+});
+
+userSchema.methods.validPassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+
+        cb(null, isMatch);
+    });
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+
+    //Generate token using jsonwebtoken
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function(err, user) {
+        if(err) return cb(err);
+
+        cb(null, user);
+    });
+}
 
 const User = mongoose.model('User', userSchema)
 
